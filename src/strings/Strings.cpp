c1ff4ce1975b2da48cc9a5fcd5d435336ef3b604
paradajz <2544094+paradajz@users.noreply.github.com>
2017-05-08 18:30:20 +0200

    add ability to manipulate temp string buffer

--- src/strings/Strings.cpp
+++ src/strings/Strings.cpp
@@ -1,45 +1,90 @@
 #include "Strings.h"
 
-void addNumberToCharArray(int32_t number, uint8_t &stringSize)
+void addNumberToCharArray(int32_t number, uint8_t &stringSize, bool temp)
 {
     char intToCharArray[7];
     itoa(number, intToCharArray, 10);
-    stringSize += strlen(intToCharArray);
-    if (number < 0) stringSize++;
-    strcat(stringBuffer, intToCharArray);
-    stringBuffer[stringSize] = '\0';
-}
+    stringSize += getNumberOfDigits(number);
 
-void addByteToCharArray(uint8_t byte, uint8_t &stringSize)
-{
-    stringBuffer[stringSize] = byte;
+    if (number < 0)
+        stringSize++;
 
-    stringSize++;
-    stringBuffer[stringSize] = '\0';
-}
-
-void appendText(const char *text, uint8_t &stringSize)
-{
-    stringSize += strlen(text);
-    strcat(stringBuffer, text);
-    stringBuffer[stringSize] = '\0';
-}
-
-void addSpaceToCharArray(uint8_t numberOfSpaces, uint8_t &stringSize)
-{
-    for (int i=0; i<numberOfSpaces; i++)
-        stringBuffer[stringSize+i] = ' ';
-
-    stringSize += numberOfSpaces;
+    if (temp)
+    {
+        strcat(tempBuffer, intToCharArray);
+        tempBuffer[stringSize] = '\0';
+    }
+    else
+    {
+        strcat(stringBuffer, intToCharArray);
         stringBuffer[stringSize] = '\0';
+    }
 }
 
-void startLine()
+void addByteToCharArray(uint8_t byte, uint8_t &stringSize, bool temp)
 {
-    stringBuffer[0] = '\0';
+    if (temp)
+    {
+        tempBuffer[stringSize] = byte;
+        stringSize++;
+        tempBuffer[stringSize] = '\0';
+    }
+    else
+    {
+        stringBuffer[stringSize] = byte;
+        stringSize++;
+        stringBuffer[stringSize] = '\0';
+    }
 }
 
-void endLine(uint8_t &stringSize)
+void appendText(const char *text, uint8_t &stringSize, bool temp)
 {
-    stringBuffer[stringSize] = '\0';
+    if (temp)
+    {
+        stringSize += strlen(text);
+        strcat(tempBuffer, text);
+        tempBuffer[stringSize] = '\0';
+    }
+    else
+    {
+        stringSize += strlen(text);
+        strcat(stringBuffer, text);
+        stringBuffer[stringSize] = '\0';
+    }
+}
+
+void addSpaceToCharArray(uint8_t numberOfSpaces, uint8_t &stringSize, bool temp)
+{
+    if (temp)
+    {
+        for (int i=0; i<numberOfSpaces; i++)
+            tempBuffer[stringSize+i] = ' ';
+
+        stringSize += numberOfSpaces;
+        tempBuffer[stringSize] = '\0';
+    }
+    else
+    {
+        for (int i=0; i<numberOfSpaces; i++)
+            stringBuffer[stringSize+i] = ' ';
+
+        stringSize += numberOfSpaces;
+        stringBuffer[stringSize] = '\0';
+    }
+}
+
+void startLine(bool temp)
+{
+    if (temp)
+        tempBuffer[0] = '\0';
+    else
+        stringBuffer[0] = '\0';
+}
+
+void endLine(uint8_t &stringSize, bool temp)
+{
+    if (temp)
+        tempBuffer[stringSize] = '\0';
+    else
+        stringBuffer[stringSize] = '\0';
 }
--- src/strings/Strings.h
+++ src/strings/Strings.h
@@ -14,37 +14,37 @@
 /// @param [in] number Number to append.
 /// @param [in, out] stringSize Current stringBuffer size. Size gets incremented after number is appended.
 ///
-void addNumberToCharArray(int32_t number, uint8_t &stringSize);
+void addNumberToCharArray(int32_t number, uint8_t &stringSize, bool temp = false);
 
 ///
 /// \brief Appends raw byte value to stringBuffer.
 /// @param [in] byte Byte value to append.
 /// @param [in, out] stringSize Current stringBuffer size. Size gets incremented after byte is appended.
 ///
-void addByteToCharArray(uint8_t byte, uint8_t &stringSize);
+void addByteToCharArray(uint8_t byte, uint8_t &stringSize, bool temp = false);
 
 ///
 /// \brief Appends text to stringBuffer.
 /// @param [in] text Text to append.
 /// @param [in, out] stringSize Current stringBuffer size. Size gets incremented after text is appended.
 ///
-void appendText(const char *text, uint8_t &stringSize);
+void appendText(const char *text, uint8_t &stringSize, bool temp = false);
 
 ///
 /// \brief Appends space to stringBuffer.
 /// @param [in] numberOfSpaces  Number of spaces to append to stringBuffer.
 /// @param [in, out] stringSize Current stringBuffer size. Size gets incremented after spaces are appended.
 ///
-void addSpaceToCharArray(uint8_t numberOfSpaces, uint8_t &stringSize);
+void addSpaceToCharArray(uint8_t numberOfSpaces, uint8_t &stringSize, bool temp = false);
 
 ///
 /// \brief Clears contents of stringBuffer.
 ///
-void startLine();
+void startLine(bool temp = false);
 
 ///
 /// \brief Ends stringBuffer by appending '\0' char to current index.
 ///
-void endLine(uint8_t &stringSize);
+void endLine(uint8_t &stringSize, bool temp = false);
 
 /// @}

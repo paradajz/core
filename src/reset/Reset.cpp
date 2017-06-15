ca29c2de3e50f84ac0f6b2347349daa7249b17fb
paradajz <2544094+paradajz@users.noreply.github.com>
2017-06-15 18:13:22 +0200

    wdt_init in init3, misc

--- src/reset/Reset.cpp
+++ src/reset/Reset.cpp
@@ -2,7 +2,7 @@
 
 #define WDFR 3
 
-void disablePeripherals()
+void disablePeripherals(void)
 {
     //disable eeprom
     EECR = 0;
@@ -25,27 +25,45 @@ void disablePeripherals()
     //disable timers
     TIMSK0 = 0;
     TIMSK1 = 0;
+    TIMSK2 = 0;
     TIMSK3 = 0;
+    TIMSK4 = 0;
+    TIMSK5 = 0;
 
     //disable USART
+    UCSR0B = 0;
     UCSR1B = 0;
+    UCSR2B = 0;
+    UCSR3B = 0;
 
     //disable I2C
     TWCR = 0;
 
-    //set all pins to inputs
-    DDRB = 0;
-    DDRC = 0;
-    DDRD = 0;
-    DDRE = 0;
-    DDRF = 0;
-
     //write low to all pins
+    PORTA = 0;
     PORTB = 0;
     PORTC = 0;
     PORTD = 0;
     PORTE = 0;
     PORTF = 0;
+    PORTG = 0;
+    PORTH = 0;
+    PORTJ = 0;
+    PORTK = 0;
+    PORTL = 0;
+
+    //set all pins to inputs
+    DDRA = 0;
+    DDRB = 0;
+    DDRC = 0;
+    DDRD = 0;
+    DDRE = 0;
+    DDRF = 0;
+    DDRG = 0;
+    DDRH = 0;
+    DDRJ = 0;
+    DDRK = 0;
+    DDRL = 0;
 }
 
 void mcuReset()

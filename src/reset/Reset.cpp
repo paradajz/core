d147a5f79c9d957eaa53974fa6fa841f9421be03
paradajz <2544094+paradajz@users.noreply.github.com>
2017-06-15 19:08:02 +0200

    ifdefs

--- src/reset/Reset.cpp
+++ src/reset/Reset.cpp
@@ -23,47 +23,111 @@ void disablePeripherals(void)
     ADCSRA = 0;
 
     //disable timers
+    #ifdef TIMSK0
     TIMSK0 = 0;
+    #endif
+    #ifdef TIMSK1
     TIMSK1 = 0;
+    #endif
+    #ifdef TIMSK2
     TIMSK2 = 0;
+    #endif
+    #ifdef TIMSK3
     TIMSK3 = 0;
+    #endif
+    #ifdef TIMSK4
     TIMSK4 = 0;
+    #endif
+    #ifdef TIMSK5
     TIMSK5 = 0;
+    #endif
 
     //disable USART
+    #ifdef UCSR0B
     UCSR0B = 0;
+    #endif
+    #ifdef UCSR1B
     UCSR1B = 0;
+    #endif
+    #ifdef UCSR2B
     UCSR2B = 0;
+    #endif
+    #ifdef UCSR3B
     UCSR3B = 0;
+    #endif
 
     //disable I2C
     TWCR = 0;
 
     //write low to all pins
+    #ifdef PORTA
     PORTA = 0;
+    #endif
+    #ifdef PORTB
     PORTB = 0;
+    #endif
+    #ifdef PORTC
     PORTC = 0;
+    #endif
+    #ifdef PORTD
     PORTD = 0;
+    #endif
+    #ifdef PORTE
     PORTE = 0;
+    #endif
+    #ifdef PORTF
     PORTF = 0;
+    #endif
+    #ifdef PORTG
     PORTG = 0;
+    #endif
+    #ifdef PORTH
     PORTH = 0;
+    #endif
+    #ifdef PORTJ
     PORTJ = 0;
+    #endif
+    #ifdef PORTK
     PORTK = 0;
+    #endif
+    #ifdef PORTL
     PORTL = 0;
+    #endif
 
     //set all pins to inputs
+    #ifdef DDRA
     DDRA = 0;
+    #endif
+    #ifdef DDRB
     DDRB = 0;
+    #endif
+    #ifdef DDRC
     DDRC = 0;
+    #endif
+    #ifdef DDRD
     DDRD = 0;
+    #endif
+    #ifdef DDRE
     DDRE = 0;
+    #endif
+    #ifdef DDRF
     DDRF = 0;
+    #endif
+    #ifdef DDRG
     DDRG = 0;
+    #endif
+    #ifdef DDRH
     DDRH = 0;
+    #endif
+    #ifdef DDRJ
     DDRJ = 0;
+    #endif
+    #ifdef DDRK
     DDRK = 0;
+    #endif
+    #ifdef DDRL
     DDRL = 0;
+    #endif
 }
 
 void mcuReset()

c6228f3eca2846970b3490ca20e2ca3b542c8825
paradajz <2544094+paradajz@users.noreply.github.com>
2017-06-28 10:01:44 +0200

    add ability to use uart0 for printf

--- src/uart/UART.cpp
+++ src/uart/UART.cpp
@@ -15,6 +15,25 @@ RingBuff_t  txBuffer;
 RingBuff_t  rxBuffer;
 #endif
 
+#ifdef USE_TX_DEBUG
+///
+/// \brief Stream used to send printf statements to UART.
+///
+static FILE uart_printf_stream;
+
+///
+/// \brief Prints a character to FILE stream
+/// @param [in] c   Character to write
+///
+int printChar(char c, FILE *stream)
+{
+    //write directly to UART
+    while ((UCSR0A & (1 << UDRE0)) == 0) {};
+    UDR0 = c;
+    return 0;
+}
+#endif
+
 ///
 /// \brief Default constructor.
 ///
@@ -73,9 +92,43 @@ ISR(USART1_UDRE_vect)
 /// \brief Initializes UART peripheral.
 /// @param [in] baudRate UART baudrate.
 ///
-void UART::begin(uint32_t baudRate)
+void UART::begin
+(uint32_t baudRate
+#ifdef DEBUG
+, uint32_t baudRate_debug
+#endif
+)
 {
-    int16_t baud_count = ((F_CPU / 8) + (baudRate / 2)) / baudRate;
+	int16_t baud_count;
+	
+	#ifdef USE_TX_DEBUG
+	UCSR0B = (1<<RXEN0) | (1<<TXEN0);
+
+    uart_printf_stream.put   = printChar;
+    uart_printf_stream.get   = NULL;
+    uart_printf_stream.flags = _FDEV_SETUP_WRITE;
+    uart_printf_stream.udata = 0;
+    stdout = &uart_printf_stream;
+
+    baud_count = ((F_CPU / 8) + (baudRate_debug / 2)) / baudRate_debug;
+
+    if ((baud_count & 1) && baud_count <= 4096)
+    {
+        //double speed uart
+        UCSR0A = (1<<U2X0);
+        UBRR0 = baud_count - 1;
+    }
+    else
+    {
+        UCSR0A = 0;
+        UBRR0 = (baud_count >> 1) - 1;
+    }
+
+    //8 bit, no parity, 1 stop bit
+    UCSR0C = (1<<UCSZ00) | (1<<UCSZ01);
+	#endif
+	
+	baud_count = ((F_CPU / 8) + (baudRate / 2)) / baudRate;
 
     if ((baud_count & 1) && baud_count <= 4096)
     {
--- src/uart/UART.h
+++ src/uart/UART.h
@@ -10,7 +10,13 @@ class UART
 {
     public:
     UART();
-    void begin(uint32_t baudRate);
+    void begin
+	(
+	uint32_t baudRate
+	#ifdef USE_TX_DEBUG
+	, uint32_t baudRate_debug
+	#endif
+	);
     bool available();
     int16_t read();
     int8_t write(uint8_t data);

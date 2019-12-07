d18dffd5d6a6e2a3becdf7d4a4962fa29ab9f728
paradajz <2544094+paradajz@users.noreply.github.com>
2019-12-07 17:29:58 +0100

    let user define ADC functionality for STM32

--- src/arch/stm32/ADC.h
+++ src/arch/stm32/ADC.h
@@ -28,80 +28,10 @@ namespace core
 {
     namespace adc
     {
-        enum class prescaler_t : uint8_t
-        {
-            p128 = 128,
-            p64 = 64,
-            p32 = 32,
-            p16 = 16
-        };
-
-        enum class vRef_t : uint8_t
-        {
-            aref = 0,
-            avcc = 1,
-            int2v56 = 2,
-            int1v1 = 3
-        };
-
-        ///
-        /// \brief Structure holding ADC prescaler and voltage reference settings.
-        ///
-        typedef struct
-        {
-            prescaler_t prescaler;
-            vRef_t      vref;
-        } conf_t;
-
-        ///
-        /// \brief Starts ADC conversion by setting ADSC bit in ADCSRA register.
-        ///
-        inline void startConversion()
-        {
-        }
-
-        ///
-        /// \brief Enables ADC interrupts by setting ADIE bit in ADCSRA register.
-        ///
-        inline void enableInterrupt()
-        {
-        }
-
-        ///
-        /// \brief Sets up ADC.
-        /// @param [in] configuration Prescaler and analog voltage reference settings. See adcConf_t structure.
-        ///
-        inline void setup(conf_t configuration)
-        {
-            
-        }
-
-        ///
-        /// \brief Get ADC value from last set ADC channel.
-        /// Blocking function.
-        /// \returns Value from ADC registers (ADCH and ADCL).
-        ///
-        inline uint16_t read()
-        {
-            return 0;
-        }
-
-        ///
-        /// \brief Disable digital input circuitry on specified ADC channel.
-        /// @param[in] adcChannel   ADC Channel.
-        ///
-        inline void disconnectDigitalIn(uint8_t adcChannel)
-        {
-            
-        }
-
-        ///
-        /// \brief Sets active ADC channel.
-        /// @param[in] adcChannel   ADC Channel.
-        ///
-        inline void setChannel(uint8_t adcChannel)
-        {
-        }
+        //user should provide these functions due to much more complex ADC peripheral than on AVR
+        void     startConversion() __attribute__((weak));
+        void     setChannel(uint32_t adcChannel) __attribute__((weak));
+        uint16_t read() __attribute__((weak));
     }    // namespace adc
 }    // namespace core
 

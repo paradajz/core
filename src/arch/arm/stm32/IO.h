b313cf0d75f607eb540892b7aa8c1a44edd03c4e
paradajz <2544094+paradajz@users.noreply.github.com>
2022-01-08 19:19:51 +0100

    add support for stm32f401xc

--- src/arch/arm/stm32/IO.h
+++ src/arch/arm/stm32/IO.h
@@ -25,7 +25,7 @@
 #include <inttypes.h>
 #include "../../../general/Helpers.h"
 
-#if defined(STM32F407xx) || defined(STM32F405xx) || defined(STM32F401xE) || defined(STM32F411xE)
+#if defined(STM32F407xx) || defined(STM32F405xx) || defined(STM32F401xE) || defined(STM32F401xC) || defined(STM32F411xE)
 #include "stm32f4xx_hal.h"
 #elif defined(STM32F031x6)
 #include "stm32f0xx_hal.h"
